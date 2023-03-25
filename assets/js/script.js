9165b63edabd03f6000ec3ddc95a29ba95bb9990
sadums <samjadams4@gmail.com>
2023-03-25 00:26:03 -0600

    add past present future classes based on time

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,7 +26,24 @@ $(function () {
     // attribute of each time-block be used to conditionally add or remove the
     // past, present, and future classes? How can Day.js be used to get the
     // current hour in 24-hour time?
-    //
+    
+    var currentTime = dayjs().hour();
+
+    for(var i = 0; i < timeBlocks.length; i++){
+        var timeBlockHour = +timeBlocks.eq(i).attr('id').split("-")[1];
+        if(timeBlockHour < 9){
+            timeBlockHour += 12;
+        }
+        if(timeBlockHour < currentTime){
+            timeBlocks.eq(i).addClass('past');
+            break;
+        }else if(timeBlockHour === currentTime){
+            timeBlocks.eq(i).addClass('present');
+            break;
+        }else{
+            timeBlocks.eq(i).addClass('future');
+        }
+    }
     // TODO: Add code to get any user input that was saved in localStorage and set
     // the values of the corresponding textarea elements. HINT: How can the id
     // attribute of each time-block be used to do this?
